d5be064a35f911e57653b77f08e2f7ce1a26c6a4
moussaab moulim <moussaabmma@gmail.com>
2020-07-01 03:49:44 +0100

    adding property Team to the Tickets Model

---
+++ SMDAsh/Migrations/20200701024540_addt.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SMDAsh.Migrations
+{
+    public partial class addt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Team",
+                table: "Tickets",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Team",
+                table: "Tickets");
+        }
+    }
+}
